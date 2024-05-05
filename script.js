17f6c9ec595cd633ba562ca4ebdaa8345a94e48c
VPC <111203113+VinhPhmCng@users.noreply.github.com>
2024-05-05 19:49:40 +0700

    Update questions and answers

--- script.js
+++ script.js
@@ -1,182 +1,182 @@
 const questions = [
     {
-        question: "Q1/12: Test câu hỏi?",
+        question: "Q1/12: Một buổi sáng thứ Hai đầu tuần, bạn thức dậy từ một giấc mơ: Người bạn thân nhất đã phản bội bạn và đi theo người bạn ghét. Bạn sẽ làm gì?",
         image: "media/test.gif",
         answers: {
             F: {
-                text: "Test choice",
-                scores: { T: 0, F: +1 },
+                text: "Suy nghĩ về chuyện của hai đứa trong thời gian gần, liệu giấc mơ có phải điềm báo?!",
+                scores: { S: 0, N: +1 },
             },
             T: {
-                text: "Should I rest or work first when I get home? Most importantly, what's for dinner?",
-                scores: { T: +1, F: 0 },
+                text: "Tò mò về phần tiếp theo của giấc mơ, không biết có cảnh var nhau không nhỉ?",
+                scores: { S: +1, N: 0 },
             },
         },
     },
     {
-        question: "Q2/12: You look outside the windows, seems like it's going to rain soon. Suddenly everything turns into darkness!!",
+        question: "Q2/12: Đến giờ đi học rồi. Phóng trên con chiến mã lao thẳng về phía cổng trường, bạn thấy Hoàng tử Lai đang trực cổng và bắt những người đang sơ vin!?!?",
         image: "media/Q2.gif",
         answers: {
             J: {
-                text: "I don't remember this tunnel. *Pull out your phone to look at the map*",
+                text: "Ngơ ngác không hiểu gì, cứ đi vào trường như bình thường.",
                 scores: { P: 0, J: +1 },
             },
             P: {
-                text: "*Turn around to see the other's reaction* Should I ask the driver what's going on?",
+                text: "Hào hứng hưởng ứng cởi sơ vin ra ngay lập tức, trường mình đúng là sáng suốt.",
                 scores: { P: +1, J: 0 },
             },
         },
     },
     {
-        question: "Q3/12: Before you can do anything, the bus stops and everything around you floats up!",
+        question: "Q3/12: Giờ chào cờ tới rồi, vẫn như mọi ngày, bài Quốc ca hào hùng được bật lên. Thế nhưng ngay sau đó là Yên Hòa Cypher!!!",
         image: "media/Q3.gif",
         answers: {
             S: {
-                text: "Ok this is weird, am I dreaming? *pinch your arm*",
-                scores: { S: +1, N: 0 },
-            },
-            N: {
-                text: "Is this a prank? Am I getting kidnapped by aliens?!",
-                scores: { S: 0, N: +1 },
-            },
-        },
-    },
-    {
-        question: "Q4/12: Amidst the surprise, your eyes catch something drifting past the window.",
-        image: "media/Q4.gif",
-        answers: {
-            F: {
-                text: "Woahhh...so many cats! How cute!! I wanna give them a hug!",
-                scores: { T: 0, F: +1 },
-            },
-            T: {
-                text: "Cats? In space? That's kinda odd... How do they survive out there?!",
-                scores: { T: +1, F: 0 },
-            },
-        },
-    },
-    {
-        question: "Q5/12: 'Welcome Aboard!' a small robot appears and announces, 'We will reach our destination in 5 months 6 days and 2 hours!'",
-        image: "media/Q5.gif",
-        answers: {
-            P: {
-                text: "Cool! A space cruise?! Where are we going? Do I get a spacesuit? This is so exciting!",
-                scores: { P: +1, J: 0 },
-            },
-            J: {
-                text: "Hey uh...Am I in space? How did I get here? When can I go home?! Why..",
-                scores: { P: 0, J: +1 },
-            },
-        },
-    },
-    {
-        question: "Q6/12: The robot just points to the back of the bus which now looks more like inside of a spaceship! What do you want to see first?",
-        image: "media/Q6.gif",
-        answers: {
-            I: {
-                text: "Solar system themed fountain in the middle. I want to take pictures with it.",
-                scores: { I: 0, E: +1 },
-            },
-            E: {
-                text: "Cozy corner with a massaging chair. I need to sit down and process.",
-                scores: { I: +1, E: 0 },
-            },
-        },
-    },
-    {
-        question: "Q7/12: On the way back to your seat, you are surrounded by cats! 'Unauthorized passenger detected, CAPTURE CAPTURE!'",
-        image: "media/Q7.gif",
-        answers: {
-            S: {
-                text: "Woah! What do you mean unauthorized? I'm the passenger of this cruise!",
-                scores: { S: +1, N: 0 }
-            },
-            N: {
-                text: "Ok, I have no idea what's going on. How about we calm down and chat for a bit?",
-                scores: { S: 0, N: +1 },
-            },
-        },
-    },
-    {
-        question: "Q8/12: Suddenly, the robot glides in, it whispers something to the cats that makes them stop.",
-        image: "media/Q8.gif",
-        answers: {
-            E: {
-                text: "Bob!! Thank you! You come to save me, right?",
+                text: "Ngồi tại chỗ vibing cùng đám bạn, hôm nay chào cờ thú vị ha.",
                 scores: { E: +1, I: 0 },
             },
-            I: {
-                text: "HA! That's right! Back off, cats! Am I safe to go now?!",
+            N: {
+                text: "Cảm thấy khó chịu vì mới sáng đã bật nhạc quá to.",
                 scores: { E: 0, I: +1 },
             },
         },
     },
     {
-        question: "Q9/12: The robot transforms into a giant vacuum cleaner, the cats use it to point at you!",
-        image: "media/Q9.gif",
+        question: "Q4/12: Hết giờ chào cờ và bạn lại lên lớp. Trên cầu thang bạn thân va phải một người khác và cậu ta hùng hổ quát ngược lại/lườm nguýt lẩm bẩm. Bạn sẽ",
+        image: "media/Q4.gif",
         answers: {
+            F: {
+                text: "Khuyên can bạn rồi kéo bạn đi về lớp.",
+                scores: { I: +1, E: 0 },
+            },
+            T: {
+                text: "Đứng ra bảo vệ bạn, quạc ngược lại ông bạn kia.",
+                scores: { I: 0, E: +1 },
+            },
+        },
+    },
+    {
+        question: "Q5/12: Cuối cùng cũng tới cửa lớp, bạn mở cửa bước vào. Nhưng không gian trong đó không phải lớp của bạn, và người bạn thân cũng đột nhiên biến mất. Đó là thế giới sự kiện Yên Hòa!",
+        image: "media/Q5.gif",
+        answers: {
+            P: {
+                text: "\"Nhìn quen quá, liệu có phải mình đang ở thế giới sự kiện không? Thế còn lớp học thì sao?\"",
+                scores: { N: +1, S: 0 },
+            },
             J: {
-                text: "Throw your coat at them as distraction, run towards the big green 'EXIT' sign!",
+                text: "\"Uầyy hay thế, vậy là không phải học nữa, đi chơi bời khám phá thôi!\"",
+                scores: { N: 0, S: +1 },
+            },
+        },
+    },
+    {
+        question: "Q6/12: Vừa bước chân vào sự kiện, bạn đột nhiên bị kéo lên tháp. Bộ đồng phục biến thành đồ diễn, xung quanh khán giả đang cổ vũ nhiệt liệt.",
+        image: "media/Q6.gif",
+        answers: {
+            I: {
+                text: "\“Không ổn rồi, mình có biết nhảy gì đâu. Hay là tìm cách sủi xuống mà không ai biết nhỉ?\”",
                 scores: { J: +1, P: 0 },
             },
-            P: {
-                text: "Run in random direction, confuse them, blend in with the crowd!",
+            E: {
+                text: "\“Từ trên cao nhìn xuống tuyệt quá, mọi người cũng nhiệt nữa. Thôi thì freestyle theo nhạc vậy!\”",
                 scores: { J: 0, P: +1 },
             },
         },
     },
     {
-        question: "Q10/12: You run past the gift shop. Looks like you got rid of them. It wouldn't hurt to get some souvenirs, right?",
+        question: "Q7/12: Buổi trình diễn kết thúc, đội của bạn đã vô địch. Riêng bạn MC giữ lại phát biểu cảm nghĩ. Bạn sẽ nói gì?",
+        image: "media/Q7.gif",
+        answers: {
+            S: {
+                text: "Đóng vai người trình diễn, cảm ơn mọi người, khán giả, ban giám khảo một cách trang trọng và đầy cảm xúc.",
+                scores: { I: 0, E: +1 },
+            },
+            N: {
+                text: "Ngập ngừng, ngại ngùng không biết trả lời như nào, tìm cách trốn tránh việc phỏng vấn.",
+                scores: { I: +1, E: 0 }
+            },
+        },
+    },
+    {
+        question: "Q8/12: Đang nói thì cái mic nổ tung, bạn giật mình đứng dậy giữa lớp. Tất cả mọi người nhìn chằm chằm vào bạn.",
+        image: "media/Q8.gif",
+        answers: {
+            E: {
+                text: "Vội vàng xin lỗi cô và các bạn rồi tiếp tục học trong sự bồn chồn.",
+                scores: { T: +1, F: 0 },
+            },
+            I: {
+                text: "Ngại ngùng gục xuống, chỉ muốn tìm cái hố chui vào.",
+                scores: { T: 0, F: +1 },
+            },
+        },
+    },
+    {
+        question: "Q9/12: Dường như chả ai biết gì về nơi bạn vừa bước vào. Bạn vừa suy nghĩ vừa đi xuống góc nhỏ trong giờ ra chơi.",
+        image: "media/Q9.gif",
+        answers: {
+            J: {
+                text: "Tìm người quen để kể chuyện, xem liệu có ai giống mình hay không.",
+                scores: { I: 0, E: +1 },
+            },
+            P: {
+                text: "Giữ kín mọi chuyện, tự suy nghĩ về thế giới đó một mình trước khi hỏi mọi người.",
+                scores: { I: +1, E: 0 },
+            },
+        },
+    },
+    {
+        question: "Q10/12: Một đám học sinh kéo xô vào góc nhỏ. Thì ra là canteen đang phát đồ ăn miễn phí. Có hai lựa chọn được canteen quảng cáo: “Bánh mì trí nhớ” và “Trà đường quay ngược thời gian”. Bạn sẽ chọn gì?",
         image: "media/Q10.gif",
         answers: {
             F: {
-                text: "Get the Jupiter headphone, wear it to hear people's thoughts!",
-                scores: { F: +1, T: 0 },
+                text: "Bánh mì trí nhớ.",
+                scores: { N: +1, S: 0 },
             },
             T: {
-                text: "Get the Mars watch, turn it and you can time travel!",
-                scores: { F: 0, T: +1 },
+                text: "Trà đường quay ngược thời gian.",
+                scores: { N: 0, S: +1 },
             },
         },
 
     },
     {
-        question: "Q11/12: The giant vacuum cleaner suddenly emerges in front of you! You're sucked into its vortex!!!",
+        question: "Q11/12: Đột nhiên ánh mắt bạn va vào một điều lạ thường. Một con mèo đeo trên người pompom. Một con mèo biết nói!!! Con mèo hỏi: \"Liệu bạn đã dậy thật chưa?\"",
         image: "media/Q11.gif",
         answers: {
             S: {
-                text: "It's ok, this is just a dream! I'll wake up soon! *close your eyes and give up*",
-                scores: { S: +1, N: 0 },
+                text: "“Cái quái gì vậy?!?! Trông thật kỳ quặc!”",
+                scores: { T: +1, F: 0 },
             },
             N: {
-                text: "I'll find the way out! Maybe there are some secret doors! *look around for the way out*",
-                scores: { S: 0, N: +1 },
+                text: "“Con mèo cute hay ho vậy, ra sờ thử phát.”",
+                scores: { T: 0, F: +1 },
             },
         },
     },
     {
-        question: "Q12/12: After a moment of darkness, you find yourself back in the bus. 'Are you alright?' the ticket inspector asks",
+        question: "Q12/12: Trống đánh, con mèo chạy bắn đi về phía sau góc nhỏ.",
         image: "media/Q12.gif",
         answers: {
             I: {
-                text: "Woah! I thought I was captured... Nevermind! I'm alright now, I guess? *panic in silence*",
-                scores: { E: 0, I: +1 },
+                text: "Chạy đuổi theo con mèo.",
+                scores: { J: 0, P: +1 },
             },
             E: {
-                text: "You won't believe it...I think I just had the wildest dream ever! *tell him about your space adventure*",
-                scores: { E: +1, I: 0 },
+                text: "Chạy đi về lớp, bỏ qua con mèo.",
+                scores: { J: +1, P: 0 },
             },
         },
     },
     {
-        question: "When you look up again, the inspector's already gone. You glance outside the window, darkness creeps in as the bus enters another tunnel...",
+        question: "Hết ngày rồi, bạn lững thững đi về, kết thúc một ngày trong sự ngỡ ngàng và khó hiểu.",
         image: "media/Q13.gif",
         answers: {
             K: {
-                text: "Wait...was that Bob?",
+                text: "Suy nghĩ về những gì vừa xảy ra, tự hỏi làm sao để quay lại thế giới đó.",
             },
             U: {
-                text: "Oh no...Do I have to go through this again?",
+                text: "Không suy nghĩ gì nữa, đi ăn tào phớ cho nó ngon.",
             },
         },
     },
@@ -184,52 +184,52 @@ const questions = [
 
 const resultOptions = {
     "ISTJ": {
-        image: "1DE.png",
+        image: "ISTJ.png",
     },
     "ISFJ": {
-        image: "2light.png"
+        image: "ISFJ.png"
     },
     "INFJ": {
-        image: "3UFO.png"
+        image: "INFJ.png"
     },
     "INTJ": {
-        image: "4nebula.png"
+        image: "INTJ.png"
     },
     "ISTP": {
-        image: "5comet.png"
+        image: "ISTP.png"
     },
     "ISFP": {
-        image: "6ST.png"
+        image: "ISFP.png"
     },
     "INFP": {
-        image: "7DM.png"
+        image: "INFP.png"
     },
     "INTP": {
-        image: "8met.png"
+        image: "INTP.png"
     },
     "ESTP": {
-        image: "9BH.png"
+        image: "ESTP.png"
     },
     "ESFP": {
-        image: "10Sn.png"
+        image: "ESFP.png"
     },
     "ENFP": {
-        image: "11Grav.png"
+        image: "ENFP.png"
     },
     "ENTP": {
-        image: "12hand.png"
+        image: "ENTP.png"
     },
     "ESTJ": {
-        image: "13sat.png"
+        image: "ESTJ.png"
     },
     "ESFJ": {
-        image: "14sun.png"
+        image: "ESFJ.png"
     },
     "ENFJ": {
-        image: "15gal.png"
+        image: "ENFJ.png"
     },
     "ENTJ": {
-        image: "16rocket.png"
+        image: "ENTJ.png"
     },
 
 };
